e8a89ce59bde478c8cb5abf59bb96a520eb0f73a
Dale Tristram <dale.tristram@sprinthive.com>
2017-11-20 09:21:45 +0200

    Only push whitelisted packages into specified repo

--- package.sh
+++ package.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
+rm -rf packages
 mkdir -p packages
 
-for x in $(ls -d */ | grep -v packages); do
+declare -a packages=(
+  "elasticsearch"
+  "kong"
+  "kong-cassandra"
+  "fluent-bit"
+  "zipkin"
+  "prometheus"
+  "grafana"
+  "kibana"
+  "jenkins"
+  "nexus"
+)
+
+for x in "${packages[@]}"
+do
   cd $x
   helm package . -d ../packages
   cd ..
 done
 
 cd packages
-helm repo index .
-aws --profile sh-charts s3 sync . s3://sprinthive-charts
+helm repo index . --url https://s3.eu-west-2.amazonaws.com/$1
+aws --profile sh-charts s3 sync . s3://$1
