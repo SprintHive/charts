f1e1afef5b921dfb01a8c009d6d9853caff1ec04
Marco Theart <marco.theart@sprinthive.com>
2018-01-29 12:08:45 +0200

    code cleanup

--- kong/prometheus-plugin/apply_config.sh
+++ kong/prometheus-plugin/apply_config.sh
@@ -1,4 +1,2 @@
 mkdir -p /usr/local/share/kong-extra-plugins/kong/plugins/prometheus;
 cp /tmp/prometheus-plugin-setup/*.lua /usr/local/share/kong-extra-plugins/kong/plugins/prometheus/;
-cd /usr/local/share/kong-extra-plugins/kong/plugins/prometheus/;
-chmod 755 ./*;
